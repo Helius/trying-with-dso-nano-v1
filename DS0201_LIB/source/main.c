ad63e13df29097f8ef8c9a72c4a4bea4b017a3e1
eugene <ghelius@gmail.com>
2012-02-22 21:47:57 +0700

    add ProgressBar widget and demo with it, need to optimise drawing

--- DS0201_LIB/source/main.c
+++ DS0201_LIB/source/main.c
@@ -7,6 +7,7 @@
 #include "usb_lib.h"
 #include "HW_V1_Config.h"
 #include "memory.h"
+#include "widgets.h"
 
 typedef  void (*pFunction)(void);
 const u32 __APP_VECTORS = 0x0800C000;
@@ -34,7 +35,6 @@ void main(void)
   Timer_Configuration();
   LCD_Initial();
   Clear_Screen(BLACK); 
-  Display_Logo(110,150);  
  
 /*----------Power ON Information----------*/ 
 
@@ -60,13 +60,50 @@ void main(void)
 ///* No app found, just hang */
 //  Display_Str(8, 7, RED,   PRN, "Error: No valid application found");
 	Clear_Screen (BLACK);
-	Draw_Line (10,10,200,20,BLUE);
-	Draw_Line (10,10,20,200,WHITE);
+//	Draw_Line (10,10,200,20,BLUE);
+//	Draw_Line (10,10,20,200,WHITE);
+//	Draw_Line (10,200,30,20,YEL);
+//	Draw_Line (10,200,200,180,GRN);
+//
+//	Draw_Line (80,80, 160,80, BLUE);
+//	Draw_Line (160,80,160,240, BLUE);
 
-	Draw_Line (10,200,30,20,YEL);
-	Draw_Line (10,200,200,180,GRN);
+	sWProgressBar pTacho;
+	sWProgressBar pFuel;
+	
+	WgProgressBar_SetGeometry (&pTacho, 320 - 60 - 10, 20, 60, 240-30);
+	WgProgressBar_SetGeometry (&pFuel, 10, 20, 60, 240-30);
 
-  while (1) {};
+	WgProgressBar_Draw (&pTacho);
+	WgProgressBar_Draw (&pFuel);
+
+	WgProgressBar_SetRange (&pFuel, 0, 100, 10);
+	WgProgressBar_SetRange (&pTacho, 0, 100, 10);
+	
+	WgProgressBar_SetValue (&pFuel,30);
+	WgProgressBar_SetValue (&pTacho,70);
+
+	Draw_Line (0,18,320,18,RGB(20,20,20));
+
+	WgProgressBar_Update (&pTacho);
+	WgProgressBar_Update (&pFuel);
+
+	int i = 0;
+	int y = 30;
+	int d;
+  while (1) {
+		WgProgressBar_SetValue (&pTacho,i);
+		WgProgressBar_SetValue (&pFuel,y);
+		WgProgressBar_Update (&pTacho);
+		WgProgressBar_Update (&pFuel);
+		i+=10;
+		y+=10;
+		if (i > 100)
+			i=0;
+		if (y > 100)
+			y=0;
+		Delayms (500);
+	}
 
 }
 /********************************* END OF FILE ********************************/
